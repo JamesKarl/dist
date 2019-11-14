3c8f1f7e8df76a5d69fe26ebdaae6912d330b55d
柯军 <kej@meiyibao.com>
2019-11-14 16:16:01 +0800

    update date format.

--- src/main/kotlin/com/myb/dist/utils/DateExtentions.kt
+++ src/main/kotlin/com/myb/dist/utils/DateExtentions.kt
@@ -3,7 +3,7 @@ package com.myb.dist.utils
 import java.text.SimpleDateFormat
 import java.util.*
 
-val DATE_PATTERN = SimpleDateFormat("YYYYmmDDHHmm")
+val DATE_PATTERN = SimpleDateFormat("yyyyMMddHHmmss")
 fun Date.toString(pattern: SimpleDateFormat): String {
     return pattern.format(this)
 }
